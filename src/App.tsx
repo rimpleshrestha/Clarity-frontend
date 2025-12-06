22a202724bd3ab9c58b674c7fc76d5069f67616f
rimpleshrestha <rimpleshrestha133@gmail.com>
2025-12-06 22:00:12 +0545

    added tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { Routes, Route } from "react-router";
 import Login from "./login/page";
-
-import Signup from "./signup/page";
 import HomePage from "./page";
-
+import Signup from "./signup/page";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const App = () => {
+  const queryClient = new QueryClient();
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </QueryClientProvider>
   );
 };
 
